c43b760312d9e88c07df0ab33efdcad4f73bc776
Gabriel W Rippel <gwr3294@cs.rit.edu>
2023-04-11 11:03:38 -0400

    implemented decode.c main to handle the same way as encode.c

---
+++ decode.c
@@ -0,0 +1,30 @@
+/** File: decode.c
+ *
+ * gets input from a file and outputs cyphertext to standard output.
+ *
+ * Author: Gabe Rippel gwr3294@rit.edu
+*/
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "cbc_lib.h"
+
+/// parses input to cbc_lib.c to decode.
+/// @param argc: number of cmd line arguments
+/// @param argv: the cmd line arguments.
+/// returns EXIT_SUCCESS if successful, EXIT_FAILURE if failed.
+int main(int argc, char * argv[]) {
+    if (argc == 1) {
+        fprintf(stderr, "usage: decode file-name to standard input\n");
+        return EXIT_FAILURE;
+    }
+    // same as in encode.
+    if (decode(argv[1]) == 0) {
+        fprintf(stderr, "success\n");
+        return EXIT_SUCCESS;
+    } else {
+        fprintf(stderr, "decode failed\n");
+        return EXIT_FAILURE;
+    }
+}
