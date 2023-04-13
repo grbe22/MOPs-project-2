1e516de494bbbea05ee2752ea1528bd71a38a44e
Gabriel W Rippel <gwr3294@cs.rit.edu>
2023-04-13 08:45:18 -0400

    cleaned up functions in cbc_lib, implemented basic encryption function.

--- cbc_lib.c
+++ cbc_lib.c
@@ -16,8 +16,13 @@
 static block64 key = 0x1234DeadBeefCafe; // each hex digit is 4 bits
 static const block64 INITIALIZATION_VECTOR = 0x0L; // initial IV value
 
+/// converts a byte64 element into a string.
+/// string: an array of string that should be undefined.
+/// text: the block64 being converted to a string.
 void b64_string(char * string, block64 text) {
-    strcopy(data, (char *) &txt, sizeof(long));
+    /// clears data. this way, terminated strings will have empty bytes after termination byte.
+    memset(string, '\0', sizeof(long) + 1);
+    strcopy(string, (char *) &txt, sizeof(long));
 }
 
 /// performs a bit barrel roll right to left by count bits.
@@ -34,6 +39,17 @@ block64 roll_right(block64 block, size_t count) {
 /// @return the changed block data
 block64 roll_left(block64 block, size_t count) {
     return (block << count) | (block >> (BITS_IN_BLOCK - count));    
+}
+
+/// converts plaintext blocks to encrypted blocks.
+/// repeated four times, block is shifted 10 to the left then xor'ed with key.
+/// this ensures an unreadable value unless you have the key value.
+block64 b_c_e(block64 block, block64 key) {
+    for (int i = 0; i < 4; i++) {
+        block = roll_left(block, 10);
+        block = block ^ key;
+    } 
+    return block;
 }
 
 int encode(const char * destpath) {
