f2685047502bb54645739bde0dd80945364dae81
Gabriel W Rippel <gwr3294@cs.rit.edu>
2023-04-15 09:47:03 -0400

    added experimental implementation of decryption, experimenting with union to convert b64 to string instead of to_string method.

--- cbc_lib.c
+++ cbc_lib.c
@@ -10,20 +10,25 @@
 #include <stdlib.h>
 #include "cbc_lib.h"
 
-#define BITS_IN_BLOCK (sizeof( long ) * 8)
+#define BITS_IN_BLOCK (sizeof(long) * 8)
 #define INPUT_SIZE (sizeof(long) * 24)
 
 static block64 key = 0x1234DeadBeefCafe; // each hex digit is 4 bits
 static const block64 INITIALIZATION_VECTOR = 0x0L; // initial IV value
 
+union block64_string {
+    char string[sizeof(long)];
+    block64 block;
+}
+
 /// converts a byte64 element into a string.
 /// string: an array of string that should be undefined.
 /// text: the block64 being converted to a string.
-void b64_string(char * string, block64 text) {
+///void b64_string(char * string, block64 text) {
     /// clears data. this way, terminated strings will have empty bytes after termination byte.
-    memset(string, '\0', sizeof(long) + 1);
-    strcopy(string, (char *) &txt, sizeof(long));
-}
+    ///memset(string, '\0', sizeof(long) + 1);
+    ///strcopy(string, (char *) &txt, sizeof(long));
+///}
 
 /// performs a bit barrel roll right to left by count bits.
 /// @param block the block64 subjected to roll.
@@ -67,6 +72,35 @@ int encode(const char * destpath) {
     return 1;
 }
 
+b64_string * cbc_decrypt(block64 * cipher, int count, block64 key, block64 * vector) {
+    b64_string * text = (char *) malloc(count + 1);
+    // scrubs any data stored in text.
+    memset(text, '\0', count + 1);
+    for (int i = 0; i < count; i ++) {
+        text.block = block_cipher_decrypt(cipher[i], key) ^ vector;
+        vector = cipher[i]
+    }
+    return(text);
+}
+
 int decode(const char * sourcepath) {
-    return 1;
+    FILE * file;
+    if ((fp = fopen(sourcepath, "rb")) == NULL) {
+        return EXIT_FAILURE;
+    }
+
+    block64 * block;
+    block = (block64 *) malloc(sizeof(block64));
+    block64 vector = INITIALIZATION_VECTOR;
+    int c = 0;
+
+    while (c = fread(block, sizeof(block64), file)) {
+        b64_string * decryption = cbc_decrypt(&block[0], count, key, vector);
+        printf("%s", decryption.string);
+        free(decryption);
+    }
+
+    free(block);
+    fclose(file);
+    return EXIT_SUCCESS;
 }
